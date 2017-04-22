4d5b246a7f062758d8279a45d6a99bb3849f29d2
robreeves <rreeves@snaplogic.com>
2017-04-21 21:02:43 -0700

    logging

--- agreedlock/src/main/java/com/robertpreeves/lock/Run.java
+++ agreedlock/src/main/java/com/robertpreeves/lock/Run.java
@@ -4,12 +4,20 @@ import com.robertpreeves.agreed.AgreedNode;
 import com.robertpreeves.agreed.AgreedNodeEndpoint;
 import com.robertpreeves.agreed.AgreedNodeFactory;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.status.StatusLogger;
+
 import java.util.Arrays;
 
 public class Run {
     public static void main(String[] args) {
+        StatusLogger.getLogger().setLevel(Level.DEBUG);
+
         AgreedNode<LockState> agreeNode = AgreedNodeFactory.create(Arrays.asList(new
                 AgreedNodeEndpoint("localhost", 8111, 1)));
+
+
+
 
         //LockNode lockNode = new LockNode(agreeNode);
 

ba280b2d181497789f3ee09199fb77b6a8c56ff6
robreeves <rreeves@snaplogic.com>
2017-04-21 20:21:06 -0700

    pom changes mainly

--- agreedcore/src/main/java/com/robertpreeves/agreed/AgreedNodeEndpoint.java
+++ agreedcore/src/main/java/com/robertpreeves/agreed/AgreedNodeEndpoint.java
@@ -3,9 +3,15 @@ package com.robertpreeves.agreed;
 public class AgreedNodeEndpoint {
     private final String hostname;
     private final int port;
+    private final int priority;
 
-    public AgreedNodeEndpoint(String hostname, int port) {
+    public AgreedNodeEndpoint(String hostname, int port, int priority) {
         this.hostname = hostname;
         this.port = port;
+        this.priority = priority;
+    }
+
+    public String getId() {
+        return String.format("%s:%s", hostname, port);
     }
 }
--- agreedcore/src/main/java/com/robertpreeves/agreed/AgreedNodeImpl.java
+++ agreedcore/src/main/java/com/robertpreeves/agreed/AgreedNodeImpl.java
@@ -1,13 +1,26 @@
 package com.robertpreeves.agreed;
 
+import com.robertpreeves.agreed.observer.Observer;
+
+import java.util.ArrayList;
 import java.util.List;
 
 class AgreedNodeImpl<T> implements AgreedNode<T> {
+    private List<Observer<T>> observers = new ArrayList<Observer<T>>();
+
     public AgreedNodeImpl(List<AgreedNodeEndpoint> otherNodes) {
         //todo
     }
 
     public void propose(T value) {
         //todo
+    }
+
+    public void subscribe(Observer<T> observer) {
+        observers.add(observer);
+    }
+
+    private void notify(final T value) {
+        observers.forEach(observer -> observer.notify(value));
     }
 }

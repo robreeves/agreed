6cb96f0616755c03d12fbb83b11fd9ae6a966022
robreeves <rreeves@snaplogic.com>
2017-04-22 17:41:09 -0700

    fleshing out rough api usage

--- agreedlock/src/main/java/com/robertpreeves/lock/Run.java
+++ agreedlock/src/main/java/com/robertpreeves/lock/Run.java
@@ -16,6 +16,16 @@ public class Run {
         AgreedNode<LockState> agreeNode = AgreedNodeFactory.create(Arrays.asList(new
                 AgreedNodeEndpoint("localhost", 8111, 1)));
 
+        //A lock subscriber will subscribe to the agreeNode
+        //It will be responsible for maintaining all lock states
+        //agreeNode.subscribe(todo);
+
+        //REST API will take a file name and contents to append
+        //It will check the lock state. If it is free then it will propose a new value
+        //If it is not free it will reject the request
+        //agreeNode.propose(todo);
+        //after the propose step is done then it will check the lock again and see if
+        //the nodes agreed to give it the lock
 
 
 
